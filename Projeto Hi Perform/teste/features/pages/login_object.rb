1698b17d8f50fdc056bab8b0b27619b21c7c050c
Ramon Luz <34165780+RamonLuz@users.noreply.github.com>
2022-09-22 16:08:22 -0300

    Delete Projeto Hi Perform/teste directory

--- Projeto Hi Perform/teste/features/pages/login_object.rb
+++
@@ -1,8 +0,0 @@
-class LoginObject < SitePrism::Page
-    set_url '/minha-conta/'   
-    
-  element  :email, '#username'
-  element  :password, '#password'
-  element  :btn_login, 'form > input.button'
-  element  :welcome, 'li:nth-child(1) > a > span'
-end
